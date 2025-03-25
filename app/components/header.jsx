fa8e6cd5e12ce24cad26a917b1600e995ef1cce8
zuuw <codymcleod02@gmail.com>
2025-03-24 21:11:57 -0600

    Added new font from google and applied it to the header.

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,12 +1,21 @@
 "use client"; 
 import Link from 'next/link';
+import { Rubik } from "next/font/google";
+
+const rubikFont = Rubik({
+  subsets:["latin"],
+  weight:"900",
+  variable:"--font-rubik",
+});
 
 export default function Header() {
   return (
+    <div className={`${rubikFont.variable}`}>
     <header className="bg-teal-500 py-6 text-white text-center">
       <Link href="/">
-        <h1 className="text-4xl font-bold">Pet Lovers' Paradise</h1>
+        <h1 className={'text-4xl font-bold font-head'}>Pet Lovers' Paradise</h1>
       </Link>
     </header>
+    </div>
   );
 }
